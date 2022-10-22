0306c84548f7535fc784c21cdd0308779f7e6620
Bloodstone2179 <87939217+Bloodstone2179@users.noreply.github.com>
2022-10-22 13:37:26 +0100

    Update pgen.py

--- pgen.py
+++ pgen.py
@@ -1,4 +1,3 @@
-import multiprocessing as mp
 def generate(arr, i, s, len):
     global pAmt
     # base case
@@ -40,14 +39,7 @@ pAmt = 0
 arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z','@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<']
 
 len = len(arr)
-process = []
-for i in range(0,10):
-    p = mp.Process(target=crack,args=(arr,len))
-    process.append(p)
-    p.start()
-
-for proc in process:
-    proc.join()
+crack(arr,len)
 
 
 
